10a0cc1e0f45d72e8aaac5c327ccccb3b67cda22
Piotr Rogulski <rivinek@gmail.com>
2019-02-06 14:47:31 +0100

    PMT-649 app.yaml render script and update

---
+++ scripts/render_app_yaml.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+
+import re
+import yaml
+
+
+def render_yaml(file_name):
+    with open(file_name) as src:
+        app = yaml.load(src.read())
+
+    for key in app['env_variables']:
+        try:
+            app['env_variables'][key] = os.environ[key]
+        except KeyError:
+            pass
+
+    app = yaml.dump(app, default_flow_style=False)
+    app = re.sub(r'\n([a-z])', r'\n\n\1', app)
+    with open(file_name, 'w') as config_file:
+        config_file.write(app)
+
+
+if '__main__' == __name__:
+
+    # TODO: Update list of yaml files to be populated
+    for file_name in ('image_service/app.yaml', ):
+        render_yaml(file_name)
